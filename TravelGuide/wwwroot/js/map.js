08f05d9ef53bce3ef6396a48220e4726d940add2
user <lambert>
2022-11-29 13:35:31 +0300

    search menu start

--- TravelGuide/wwwroot/js/map.js
+++ TravelGuide/wwwroot/js/map.js
@@ -7,34 +7,32 @@
 };
 
 
+function AddAllMarkers() {
+    fetch("https://localhost:7201/Main/GetJson")
+        .then(response => response.json())
+        .then(placemarks => {
+            placemarks.forEach(place => {
+                const map = new mapgl.Map('map', {
+                    center: [47.504771, 42.98283],
+                    zoom: 13,
+                    key: "d31240ac-6937-4994-9e2e-a9a3678b6807"
+                });
 
-fetch("https://localhost:7201/Main/GetJson")
-    .then(response => response.json())
-    .then(placemarks => {
-        const map = new mapgl.Map('map', {
-            center: [47.504771, 42.98283],
-            zoom: 13,
-            key: "d31240ac-6937-4994-9e2e-a9a3678b6807"
+                places[place.Type].push(
+                    new mapgl.CircleMarker(map, {
+                        coordinates: [place.Longitude, place.Latitude],
+                        radius: 14,
+                        color: GetPlaceColor(place.Type),
+                        strokeWidth: 4,
+                        strokeColor: '#ffffff',
+                        stroke2Width: 6,
+                        stroke2Color: GetPlaceColor(place.Type) + "55",
+                    })
+                );
+            });
         });
+}
 
-        placemarks.forEach(place => {
-            places[place.Type].push(
-                new mapgl.CircleMarker(map, {
-                    coordinates: [place.Longitude, place.Latitude],
-                    radius: 14,
-                    color: GetPlaceColor(place.Type),
-                    strokeWidth: 4,
-                    strokeColor: '#ffffff',
-                    stroke2Width: 6,
-                    stroke2Color: GetPlaceColor(place.Type) + "55",
-                })
-            );
-        });
-
-        
-              
-            
-    });
 
         
 function GetPlaceColor(type) {
@@ -54,6 +52,7 @@ function GetPlaceColor(type) {
 
 
 
+AddAllMarkers();
 
  
 
@@ -64,7 +63,6 @@ let menuOn = document.querySelector(".menu__on")
 let menuOff = document.querySelector(".menu__off")
 let menuBtn = document.querySelector(".menu__btn");
 let menu = document.querySelector(".menu");
-let map = document.getElementById("map");
 
 menuBtn.addEventListener("click", function () {
   if (menu.classList.contains("active")) {
@@ -96,6 +94,23 @@ placesBlock.forEach(item => {
         }
     });
     
+});
+
+// View All
+const view = document.querySelector(".view__all");
+view.addEventListener("click", () => {
+    for (let key in places) {
+        for (let i = 0; i < places[key].length; i++) {
+            places[key][i].destroy();
+        }
+        places[key] = [];
+    }
+    AddAllMarkers();
+});
+
+const search = document.getElementById("search");
+search.addEventListener("click", () => {
+
 });
 
 //hotelCheckbox.addEventListener("click", () => {
