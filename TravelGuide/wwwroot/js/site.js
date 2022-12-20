ab764a6eddd2dd7e20208e7ca485e3b8324468f7
123 <user@DESKTOP-VUSU881>
2022-12-21 01:57:42 +0300

    add site.js Place layaout

--- TravelGuide/wwwroot/js/site.js
+++ TravelGuide/wwwroot/js/site.js
@@ -86,3 +86,33 @@ const map = new mapgl.Map('map', {
 	key: "d31240ac-6937-4994-9e2e-a9a3678b6807"
 });
 
+
+const curseList = document.querySelectorAll('.curse');
+const desc = document.querySelector('.choose__desc');
+const comm = document.querySelector('.choose__comm');
+
+
+
+console.log(desc);
+
+
+desc.addEventListener('click', function () {
+	curseList[1].classList.remove('curse__active');
+	curseList[0].classList.add('curse__active');
+	document.querySelector('.hotel__comm').classList.remove('animation__comm');
+	document.querySelector('.hotel__comm').classList.remove('active__comm');
+
+	document.querySelector('.hotel__about').classList.add('animation');
+	document.querySelector('.hotel__about').classList.add('active');
+});
+
+
+comm.addEventListener('click', function () {
+	curseList[0].classList.remove('curse__active')
+	curseList[1].classList.add('curse__active')
+	document.querySelector('.hotel__about').classList.remove('animation');
+	document.querySelector('.hotel__about').classList.remove('active');
+	document.querySelector('.hotel__comm').classList.add('animation__comm');
+	document.querySelector('.hotel__comm').classList.add('active__comm');
+
+});
